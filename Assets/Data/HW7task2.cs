b4e9c98e752d268d73d7432d2f8195e386a7fe67
test <>
2023-02-23 11:52:39 +0100

    ДЗ 8  начало

--- Assets/Data/HW7task2.cs
+++ Assets/Data/HW7task2.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class HW7task2 : MonoBehaviour
 {
     void Start()
-    {
+    {   // home work 8
+        //Debug.Log("1 " + Application.dataPath);
+        //Debug.Log("2 " + Application.persistentDataPath); // часто этот путь исп.для хранения сейвов
+        //Debug.Log("3 " + Application.streamingAssetsPath); // файлы подкачки/временные файлы
+        //Debug.Log("4 " + Application.temporaryCachePath);
+
+        var savePath = Path.Combine(Application.dataPath, "sAvE.txt");
+        Debug.LogWarning(savePath);
+        Save(savePath, "some save data...");
+
+        Debug.Log(Load(savePath));
+        return;
+
+        // home work 7
         string str1 = "dfsdfgwe32rqw24оривr";
-        MyExtensions.FindingLengthtOfString(str1);
+        // MyExtensions.FindingLengthtOfString(str1);
     }
 
+    public string Load(string savePath = null)
+    {
+        var result = "";
+        using (var sr = new StreamReader(savePath))
+        {
+            while (!sr.EndOfStream)
+            {
+                result += sr.ReadLine();
+            }
+            return result;
+        }
+    }
 
+    private static void Save(string savePath, string data)
+    {
+        using (var sw = new StreamWriter(savePath))
+        {
+            sw.WriteLine(data);
+        }
+    }
 }
 public static class MyExtensions
 {
